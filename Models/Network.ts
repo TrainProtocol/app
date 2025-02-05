c57abd05ab4703cff0ecbc7948c2b73c71618028
babkenmes <mesropyan.b@gmail.com>
2025-02-05 16:07:59 +0400

    Added network  group types

--- Models/Network.ts
+++ Models/Network.ts
@@ -16,11 +16,14 @@ export class Network {
     logo: string;
     chain_id: string | null;
     // type: NetworkType;
-    group: string
+    group: NetworkGroup
     transaction_explorer_template: string;
     account_explorer_template: string;
     tokens: Token[]
-    native_token: Token
+    native_token: Token;
+    get type(): NetworkType {
+        return NETWORK_GROUP_TYPES[this.group];
+    }
     listing_date: string;
     nodes: [
         {
@@ -62,4 +65,61 @@ export class Token {
     price_in_usd: number;
     precision: number;
     listing_date: string;
+}
+
+const NETWORK_GROUP_TYPES = {
+    EVM_LEGACY: NetworkType.EVM,
+    EVM_EIP1559: NetworkType.EVM,
+    EVM_ARBITRUM_LEGACY: NetworkType.EVM,
+    EVM_ARBITRUM_EIP1559: NetworkType.EVM,
+    EVM_OPTIMISM_EIP1559: NetworkType.EVM,
+    EVM_OPTIMISM_LEGACY: NetworkType.EVM,
+    EVM_POLYGON_LEGACY: NetworkType.EVM,
+    EVM_POLYGON_EIP1559: NetworkType.EVM,
+    LOOPRING: NetworkType.EVM,
+
+    IMMUTABLEX: NetworkType.EVM,
+    BRINE: NetworkType.EVM,
+    RHINOFI: NetworkType.EVM,
+    APTOS: NetworkType.EVM,
+
+    FUEL: NetworkType.Fuel,
+    SOLANA: NetworkType.Solana,
+    STARKNET: NetworkType.Starknet,
+    STARKNET_PARADEX: NetworkType.Starknet,
+
+    TON: NetworkType.TON,
+    TRON: NetworkType.TON,
+    ZKSYNC: NetworkType.ZkSyncLite,
+
+    ZKSPACE: NetworkType.ZkSyncLite,
+    ZKSYNC_ERA_PAYMASTER: NetworkType.ZkSyncLite,
+    
+    OSMOSIS: NetworkType.Cosmos,
+}
+
+export enum NetworkGroup {
+    EVM_LEGACY = "EVM_LEGACY",
+    EVM_EIP1559 = "EVM_EIP1559",
+    EVM_ARBITRUM_LEGACY = "EVM_ARBITRUM_LEGACY",
+    EVM_ARBITRUM_EIP1559 = "EVM_ARBITRUM_EIP1559",
+    EVM_OPTIMISM_EIP1559 = "EVM_OPTIMISM_EIP1559",
+    EVM_OPTIMISM_LEGACY = "EVM_OPTIMISM_LEGACY",
+    EVM_POLYGON_LEGACY = "EVM_POLYGON_LEGACY",
+    EVM_POLYGON_EIP1559 = "EVM_POLYGON_EIP1559",
+    FUEL = "FUEL",
+    IMMUTABLEX = "IMMUTABLEX",
+    LOOPRING = "LOOPRING",
+    OSMOSIS = "OSMOSIS",
+    SOLANA = "SOLANA",
+    STARKNET = "STARKNET",
+    STARKNET_PARADEX = "STARKNET_PARADEX",
+    TON = "TON",
+    TRON = "TRON",
+    ZKSYNC = "ZKSYNC",
+    BRINE = "BRINE",
+    RHINOFI = "RHINOFI",
+    APTOS = "APTOS",
+    ZKSPACE = "ZKSPACE",
+    ZKSYNC_ERA_PAYMASTER = "ZKSYNC_ERA_PAYMASTER",
 }
