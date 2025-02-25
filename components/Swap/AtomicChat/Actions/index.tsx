7c3588ab6541bef913a14b71bd2b5d7f2fa8f66d
Aren <aren@bransfer.io>
2025-02-25 19:57:54 +0400

    refactor Error component to simplify rendering logic and enhance visual feedback

--- components/Swap/AtomicChat/Actions/index.tsx
+++ components/Swap/AtomicChat/Actions/index.tsx
@@ -61,38 +61,36 @@ const Error: FC = () => {
         <AnimatePresence>
             {
                 error &&
-                <ReactPortal wrapperId="widget_root">
+                <>
                     <motion.div
-                        className={`absolute inset-0 z-20 bg-black/50 block`}
-                        initial={{ opacity: 0 }}
-                        animate={{ opacity: 1 }}
-                        exit={{ opacity: 0 }}
-                    />
-                </ReactPortal>
-            }
-        </AnimatePresence>
-
-        <AnimatePresence>
-            {
-                error &&
-                <motion.div
-                    initial={{ y: 150 }}
-                    animate={{ y: 0 }}
-                    exit={{ y: 150 }}
-                    transition={{ duration: 0.15, bounceDamping: 0 }}
-                    className="absolute z-30 bottom-0 bg-secondary-700 rounded-2xl p-3 w-full"
-                >
-                    <div className="w-full space-y-3 flex flex-col justify-between h-full text-secondary-text">
-                        <TransactionMessage error={error} isLoading={false} />
-                        <button
-                            type="button"
+                        initial={{ y: 150 }}
+                        animate={{ y: 0 }}
+                        exit={{ y: 150 }}
+                        transition={{ duration: 0.15, bounceDamping: 0 }}
+                        className="absolute z-30 bottom-0 bg-secondary-700 rounded-2xl p-3 w-full shadow-card"
+                    >
+                        <div className="w-full space-y-3 flex flex-col justify-between h-full text-secondary-text">
+                            <TransactionMessage error={error} isLoading={false} />
+                            <button
+                                type="button"
+                                onClick={() => updateCommit('error', undefined)}
+                                className='relative w-full font-semibold rounded-componentRoundness transition duration-200 ease-in-out bg-secondary-400 border border-secondary-500 text-primary-text py-3 px-2 md:px-3'
+                            >
+                                Try again
+                            </button>
+                        </div>
+                    </motion.div>
+                    {/* <ReactPortal wrapperId="widget">
+                        <motion.div
+                            className={`absolute inset-0 z-20 bg-black/50 block`}
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            exit={{ opacity: 0 }}
                             onClick={() => updateCommit('error', undefined)}
-                            className='relative w-full font-semibold rounded-componentRoundness transition duration-200 ease-in-out bg-secondary-400 border border-secondary-500 text-primary-text py-3 px-2 md:px-3'
-                        >
-                            Try again
-                        </button>
-                    </div>
-                </motion.div>
+                        />
+                    </ReactPortal> */}
+                </>
+
             }
         </AnimatePresence>
     </>
