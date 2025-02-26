8028420a5a033345285fc75a21f2407fac70e2aa
paur94 <paruyryeprikyan9@gmail.com>
2025-02-26 19:24:47 +0400

    fixed clip paths

--- components/utils/pulse.tsx
+++ components/utils/pulse.tsx
@@ -6,33 +6,33 @@ const PulsatingCircles: React.FC = () => {
 
     return (
         <div className="absolute inset-0 hidden md:flex items-center justify-center mx-4">
-            {isActive && (
+            {!isActive && (
                 <>
                     <div
                         className="absolute w-[130%] aspect-square border border-secondary-text rounded-full animate-pulsate opacity-25"
                         style={{
-                            animationDelay: "0s", clipPath: "inset(0 -8px 70% -8px)", boxShadow: `
+                            animationDelay: "0s", clipPath: "inset(0 -8px 65% -8px)", boxShadow: `
                             rgba(128, 128, 128, 0.7) 6px -4px 14px -2px, rgba(128, 128, 128, 0.7) -5px 1px 12px 0px, rgba(128, 128, 128, 0.7) 6px 0px 11px 1px inset, rgba(128, 128, 128, 0.7) -6px 0px 11px 1px inset 
                         ` }}
                     ></div>
                     <div
                         className="absolute w-[150%] aspect-square border border-secondary-text rounded-full animate-pulsate opacity-25"
                         style={{
-                            animationDelay: "0.6s", clipPath: "inset(0 -8px 60% -8px)", boxShadow: `
+                            animationDelay: "0.6s", clipPath: "inset(0 -8px 58% -8px)", boxShadow: `
                             rgba(128, 128, 128, 0.7) 6px -4px 14px -2px, rgba(128, 128, 128, 0.7) -5px 1px 12px 0px, rgba(128, 128, 128, 0.7) 6px 0px 11px 1px inset, rgba(128, 128, 128, 0.7) -6px 0px 11px 1px inset 
                         ` }}
                     ></div>
                     <div
                         className="absolute w-[175%] aspect-square border-2 border-secondary-text rounded-full animate-pulsate opacity-25"
                         style={{
-                            animationDelay: "1.2s", clipPath: "inset(0 -15px 55% -15px)", boxShadow: `
+                            animationDelay: "1.2s", clipPath: "inset(0 -15px 53% -15px)", boxShadow: `
                             rgba(128, 128, 128, 0.7) 6px -4px 14px -2px, rgba(128, 128, 128, 0.7) -5px 1px 12px 0px, rgba(128, 128, 128, 0.7) 6px 0px 11px 1px inset, rgba(128, 128, 128, 0.7) -6px 0px 11px 1px inset 
                         ` }}
                     ></div>
                     <div
                         className="absolute w-[205%] aspect-square border-4 border-secondary-text rounded-full animate-pulsate opacity-25"
                         style={{
-                            animationDelay: "1.8s", clipPath: "inset(0 -15px 50% -15px)", boxShadow: `
+                            animationDelay: "1.8s", clipPath: "inset(0 -15px 48% -15px)", boxShadow: `
                             rgba(128, 128, 128, 0.7) 6px -4px 14px -2px, rgba(128, 128, 128, 0.7) -5px 1px 12px 0px, rgba(128, 128, 128, 0.7) 6px 0px 11px 1px inset, rgba(128, 128, 128, 0.7) -6px 0px 11px 1px inset 
                         ` }}
                     ></div>
@@ -45,7 +45,7 @@ const PulsatingCircles: React.FC = () => {
                     ></div>
                 </>
             )}
-            {!isActive && (
+            {isActive && (
                 <>
                     <div
                         className="absolute w-[130%] aspect-square border border-secondary-text rounded-full opacity-25"
