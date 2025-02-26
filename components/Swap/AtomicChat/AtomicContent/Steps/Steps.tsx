d0b2e0df59123095d11633fea6dbcdfc8f86d0f9
Aren <aren@bransfer.io>
2025-02-26 16:58:08 +0400

    refactor Steps component to streamline rendering logic and improve code clarity

--- components/Swap/AtomicChat/AtomicContent/Steps/Steps.tsx
+++ components/Swap/AtomicChat/AtomicContent/Steps/Steps.tsx
@@ -3,14 +3,11 @@ import { CommitStatus, useAtomicState } from "../../../../../context/atomicConte
 import { CommitTransaction } from "../../../../../lib/layerSwapApiClient";
 import LockIcon from "../../../../Icons/LockIcon";
 import Link from "next/link";
-import shortenAddress from "../../../../utils/ShortenAddress";
 import Step from "./Step";
-import { Check, Clock, Link2 } from "lucide-react";
+import { Clock, Link2 } from "lucide-react";
 import CheckedIcon from "../../../../Icons/CheckedIcon";
 import XCircle from "../../../../Icons/CircleX";
 import TimelockTimer from "../../Timer";
-import clsx from "clsx";
-import { motion } from "framer-motion";
 
 export const RequestStep: FC = () => {
     const { sourceDetails, commitId, commitTxId, source_network, commitFromApi } = useAtomicState()
@@ -34,28 +31,15 @@ export const RequestStep: FC = () => {
 }
 
 export const SignAndConfirmStep: FC = () => {
-    const { sourceDetails, destinationDetails, source_network, destination_network, commitFromApi, commitStatus } = useAtomicState()
+    const { sourceDetails, destinationDetails, destination_network, commitFromApi, commitStatus } = useAtomicState()
 
     const lpLockTx = commitFromApi?.transactions.find(t => t.type === CommitTransaction.HTLCLock)
     const lpRedeemTransaction = commitFromApi?.transactions.find(t => t.type === CommitTransaction.HTLCRedeem && t.network === destination_network?.name)
-    const addLockSigTx = commitFromApi?.transactions.find(t => t.type === CommitTransaction.HTLCAddLockSig)
-    const commited = (sourceDetails || lpLockTx) ? true : false;
 
     const assetsLocked = !!(sourceDetails?.hashlock && destinationDetails?.hashlock) || commitStatus === CommitStatus.AssetsLocked || commitStatus === CommitStatus.RedeemCompleted;
     const loading = commitStatus === CommitStatus.UserLocked
 
     const title = assetsLocked ? "Finalized" : "Finalize"
-    // const description = (assetsLocked)
-    //     ? <div className="inline-flex gap-3">
-    //         <div className="inline-flex gap-1 items-center">
-    //             <p>Solver:</p> {(lpLockTx && destination_network) ? <Link className="underline hover:no-underline" target="_blank" href={destination_network?.transaction_explorer_template.replace('{0}', lpLockTx?.hash)}>{shortenAddress(lpLockTx.hash)}</Link> : <div className="h-3 w-10 bg-gray-400 animate-pulse rounded" />}
-    //         </div>
-    //         <div className="inline-flex gap-1 items-center">
-    //             <p>You:</p> {(addLockSigTx && source_network) ? <Link className="underline hover:no-underline" target="_blank" href={source_network?.transaction_explorer_template.replace('{0}', addLockSigTx?.hash)}>{shortenAddress(addLockSigTx.hash)}</Link> : <div className="h-3 w-10 bg-gray-400 animate-pulse rounded" />}
-    //         </div>
-    //     </div>
-    //     : <>Sign and finalize the swap, you can cancel and refund anytime before.</>
-
     const completed = !!(sourceDetails?.hashlock && destinationDetails?.hashlock) || !!lpRedeemTransaction?.hash || commitStatus === CommitStatus.RedeemCompleted || commitStatus === CommitStatus.AssetsLocked
 
     const description = assetsLocked
@@ -82,11 +66,7 @@ export const SignAndConfirmStep: FC = () => {
 
 
 const SolverStatus: FC = () => {
-    const { commitId, destinationDetails, commitFromApi } = useAtomicState()
-
-    const lpLockTx = commitFromApi?.transactions.find(t => t.type === CommitTransaction.HTLCLock)
-
-    const commited = commitId ? true : false;
+    const { destinationDetails } = useAtomicState()
     const lpLockDetected = destinationDetails?.hashlock ? true : false;
 
     if (lpLockDetected) {
@@ -121,50 +101,58 @@ export const LpLockingAssets: FC = () => {
 
     return (
         commitStatus !== CommitStatus.TimelockExpired &&
-        <div className={`relative inline-flex items-center justify-between w-full bg-secondary-700 rounded-2xl p-3 ${!sourceDetails ? 'opacity-60' : ''}`}>
-            <div className="space-y-2">
-                <div className="inline-flex items-center gap-2 h-[19px]">
-                    {
-                        completed &&
-                        <CheckedIcon className="h-5 w-5 text-accent" />
-                    }
-                    {
-                        !loading && !completed &&
-                        <Clock className="w-5 h-5 text-secondary-text" />
-                    }
-                    {
-                        loading &&
-                        <div className="flex justify-center">
-                            <div className="relative flex items-center justify-end w-[18px] h-[18px] overflow-hidden border-2 border-accent rounded-full ">
-                                <div className="absolute w-1/2 h-0.5  origin-left animate-spin-fast">
-                                    <div className="w-3/4 h-full bg-accent rounded-full" />
-                                </div>
+        <>
+            <div className={`relative inline-flex items-center justify-between w-full bg-secondary-700 rounded-2xl p-3 ${!sourceDetails ? 'opacity-60' : ''}`}>
+                <div className="space-y-1">
+                    <div className="inline-flex items-center gap-2">
+                        <div className="flex w-fit items-center justify-center h-[19px]">
+                            <div
+                                className="z-10 flex w-full items-center overflow-hidden rounded-full p-0.5"
+                            >
+                                {
+                                    completed &&
+                                    <CheckedIcon className="h-5 w-5 text-accent" />
+                                }
+                                {
+                                    !loading && !completed &&
+                                    <Clock className="w-5 h-5 text-secondary-text" />
+                                }
+                                {
+                                    loading &&
+                                    <div className="flex justify-center">
+                                        <div className="relative flex items-center justify-end w-[18px] h-[18px] overflow-hidden border-2 border-accent rounded-full ">
+                                            <div className="absolute w-1/2 h-0.5  origin-left animate-spin-fast">
+                                                <div className="w-3/4 h-full bg-accent rounded-full" />
+                                            </div>
 
-                                <div className="absolute w-1/2 h-0.5  origin-left  animate-spin-slow">
-                                    <div className="w-2/3 h-full bg-accent rounded-full" />
-                                </div>
-                                <div className="absolute flex justify-center flex-1 w-full">
-                                    <div className="w-0.5 h-0.5 bg-accent rounded-full" />
-                                </div>
+                                            <div className="absolute w-1/2 h-0.5  origin-left  animate-spin-slow">
+                                                <div className="w-2/3 h-full bg-accent rounded-full" />
+                                            </div>
+                                            <div className="absolute flex justify-center flex-1 w-full">
+                                                <div className="w-0.5 h-0.5 bg-accent rounded-full" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                }
                             </div>
                         </div>
-                    }
-                    <div className="text-primary-text text-base leading-5">{title}</div>
+                        <div className="text-primary-text text-base leading-5">{title}</div>
+                    </div>
+                    <SolverStatus />
                 </div>
-                <SolverStatus />
+                {
+                    lpLockTx && destination_network && destinationDetails?.hashlock &&
+                    <div className="absolute right-5 top-6 flex items-center gap-2 bg-secondary-500 hover:bg-secondary-600 rounded-full p-1 px-2 text-sm">
+                        <Link className="flex items-center gap-1" target="_blank" href={destination_network?.transaction_explorer_template.replace('{0}', lpLockTx?.hash)}>
+                            <p>
+                                View
+                            </p>
+                            <Link2 className="h-4 w-auto" />
+                        </Link>
+                    </div>
+                }
             </div>
-            {
-                lpLockTx && destination_network && destinationDetails?.hashlock &&
-                <div className="absolute right-5 top-6 flex items-center gap-2 bg-secondary-500 hover:bg-secondary-600 rounded-full p-1 px-2 text-sm">
-                    <Link className="flex items-center gap-1" target="_blank" href={destination_network?.transaction_explorer_template.replace('{0}', lpLockTx?.hash)}>
-                        <p>
-                            View
-                        </p>
-                        <Link2 className="h-4 w-auto" />
-                    </Link>
-                </div>
-            }
-        </div>
+        </>
     )
 }
 
