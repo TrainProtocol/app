fca53b4da3e0aa792e2d35e81c8a82f0e1efc9e6
Aren <aren@bransfer.io>
2025-03-28 13:51:39 +0400

    Add validation for chainId in settingsChains filtering

--- components/WalletProviders/Wagmi.tsx
+++ components/WalletProviders/Wagmi.tsx
@@ -29,6 +29,7 @@ function WagmiComponent({ children }: Props) {
     const settingsChains = settings?.networks
         .sort((a, b) => (NetworkSettings.KnownSettings[a.name]?.ChainOrder || Number(a.chainId)) - (NetworkSettings.KnownSettings[b.name]?.ChainOrder || Number(b.chainId)))
         .filter(net => net.group.toLowerCase().includes('evm')
+            && !isNaN(Number(net.chainId))
             && net.nodes.length > 0
             && net.nativeToken)
         .map(resolveChain).filter(isChain) as Chain[]
