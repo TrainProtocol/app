d8024601d15708082185d1561ca161f7de8c53be
Aren <aren@bransfer.io>
2025-05-21 13:40:13 +0400

    update helios

--- workers/helios/helios_ts.d.ts
+++ workers/helios/helios_ts.d.ts
@@ -235,9 +235,9 @@ export interface InitOutput {
   readonly __wbindgen_free: (a: number, b: number, c: number) => void;
   readonly __wbindgen_export_6: WebAssembly.Table;
   readonly __externref_table_dealloc: (a: number) => void;
-  readonly closure277_externref_shim: (a: number, b: number, c: any) => void;
-  readonly _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h1a810c12566889eb: (a: number, b: number) => void;
-  readonly closure221_externref_shim: (a: number, b: number, c: any, d: any) => void;
+  readonly _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h50d0234c4681181b: (a: number, b: number) => void;
+  readonly closure459_externref_shim: (a: number, b: number, c: any) => void;
+  readonly closure412_externref_shim: (a: number, b: number, c: any, d: any) => void;
   readonly __wbindgen_start: () => void;
 }
 
