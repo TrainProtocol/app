7a5c7739b4c6487002a82af6df593d914c11db0b
Aren <aren@bransfer.io>
2025-07-10 21:42:36 +0400

    change some namings

--- components/layout.tsx
+++ components/layout.tsx
@@ -108,13 +108,13 @@ export default function Layout({ children, settings, themeData }: Props) {
       <link rel="canonical" href="https://app.train.tech/" />
       <meta name="msapplication-TileColor" content="#ffffff" />
       <meta name="theme-color" content={`rgb(${themeData.secondary?.[900]})`} />
-      <meta name="description" content="Trustless bridging & swapping of crypto assets between blockchains. Transfer any amount to any network secured by a battle-tested system." />
+      <meta name="description" content="Trust-minimized bridging & swapping of crypto assets between blockchains. Transfer any amount to any network secured by a battle-tested system." />
 
       {/* Facebook Meta Tags */}
       <meta property="og:url" content={`https://app.train.tech/`} />
       <meta property="og:type" content="website" />
       <meta property="og:title" content={title} />
-      <meta property="og:description" content="Trustless bridging & swapping of crypto assets between blockchains. Transfer any amount to any network secured by a battle-tested system." />
+      <meta property="og:description" content="Trust-minimized bridging & swapping of crypto assets between blockchains. Transfer any amount to any network secured by a battle-tested system." />
       <meta property="og:image" content={`https://app.train.tech/opengraph.jpg?v=2`} />
 
       {/* Twitter Meta Tags */}
@@ -122,7 +122,7 @@ export default function Layout({ children, settings, themeData }: Props) {
       <meta property="twitter:domain" content="app.train.tech" />
       <meta property="twitter:url" content={`https://app.train.tech/`} />
       <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content="Trustless bridging & swapping of crypto assets between blockchains. Transfer any amount to any network secured by a battle-tested system." />
+      <meta name="twitter:description" content="Trust-minimized bridging & swapping of crypto assets between blockchains. Transfer any amount to any network secured by a battle-tested system." />
       <meta name="twitter:image" content={`https://app.train.tech/opengraphtw.jpg`} />
     </Head>
     {
