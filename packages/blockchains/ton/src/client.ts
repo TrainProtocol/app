a66708c896bed90e5339ac09e07282813d61a31c
Aren <aren@bransfer.io>
2026-03-13 17:52:26 +0400

    fix build

--- packages/blockchains/ton/src/client.ts
+++ packages/blockchains/ton/src/client.ts
@@ -183,7 +183,7 @@ export class TonHTLCClient extends HTLCClient {
         }
     }
 
-    async _getSolverLockDetails(params: LockParams, nodeUrl: string): Promise<LockDetails | null> {
+    async getSolverLockDetails(params: LockParams, nodeUrl: string): Promise<LockDetails | null> {
         const { id, contractAddress } = params
         const rpc = TonRpcClient.fromUrl(nodeUrl, this.apiKey)
 
