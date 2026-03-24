0f459c8cb5c0b454769a59cec3fa81b0d38e8797
Aren <aren@bransfer.io>
2026-03-24 23:19:27 +0400

    Refactor TransactionCreatedEventData: remove 'networkId' property and update related logic in useOrderStreaming for consistency

--- packages/sdk/src/api/types.ts
+++ packages/sdk/src/api/types.ts
@@ -143,7 +143,6 @@ export type OrderCreatedEventData = {
 export type TransactionCreatedEventData = {
     hashlock: string
     network: string
-    networkId: string
     transactionType: string
     transactionHash: string
 }
