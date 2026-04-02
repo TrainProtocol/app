eb8d90f98401dea099e6a7253aee971cca16d536
Aren <aren@bransfer.io>
2026-04-02 19:04:37 +0400

    refactor: streamline network settings and remove unused code
    - Removed unused deposit types and network order arrays from NetworkSettings.
    - Simplified the Wagmi component by eliminating the sorting logic for chains, enhancing readability and maintainability.

--- apps/app/lib/NetworkSettings.ts
+++ apps/app/lib/NetworkSettings.ts
@@ -1,68 +1,18 @@
 import KnownInternalNames from "./knownIds";
 
-export enum DepositType {
-    Manual = 'manual',
-    Wallet = 'wallet'
-}
-
 export enum GasCalculation {
     Classic = 'classic',
     OptimismType = 'optimismType'
 }
 
-const destinationOrder = [
-    KnownInternalNames.Networks.StarkNetMainnet,
-    KnownInternalNames.Networks.ZksyncEraMainnet,
-    KnownInternalNames.Networks.ZksyncMainnet,
-    KnownInternalNames.Networks.ArbitrumNova,
-    KnownInternalNames.Networks.ArbitrumMainnet,
-    KnownInternalNames.Networks.OptimismMainnet,
-    KnownInternalNames.Networks.PolygonZkMainnet,
-    KnownInternalNames.Networks.EthereumMainnet,
-    KnownInternalNames.Networks.PolygonMainnet,
-    KnownInternalNames.Networks.AvalancheMainnet,
-    KnownInternalNames.Networks.LoopringMainnet,
-    KnownInternalNames.Networks.BNBChainMainnet,
-    KnownInternalNames.Networks.MantleMainnet,
-    KnownInternalNames.Networks.PGNMainnet,
-    KnownInternalNames.Networks.BaseMainnet,
-    KnownInternalNames.Networks.OsmosisMainnet,
-    KnownInternalNames.Networks.ZkspaceMainnet,
-    KnownInternalNames.Networks.RhinoFiMainnet,
-];
-
-const sourceOrder = [
-    KnownInternalNames.Networks.LineaMainnet,
-    KnownInternalNames.Networks.ArbitrumMainnet,
-    KnownInternalNames.Networks.EthereumMainnet,
-    KnownInternalNames.Networks.StarkNetMainnet,
-    KnownInternalNames.Networks.BNBChainMainnet,
-    KnownInternalNames.Networks.OptimismMainnet,
-    KnownInternalNames.Networks.SolanaMainnet,
-    KnownInternalNames.Networks.SolanaDevnet,
-    KnownInternalNames.Networks.ZksyncEraMainnet,
-    KnownInternalNames.Networks.PolygonMainnet,
-    KnownInternalNames.Networks.AvalancheMainnet,
-    KnownInternalNames.Networks.ZksyncMainnet,
-    KnownInternalNames.Networks.ArbitrumNova,
-    KnownInternalNames.Networks.PolygonZkMainnet,
-    KnownInternalNames.Networks.KCCMainnet,
-    KnownInternalNames.Networks.LoopringMainnet,
-    KnownInternalNames.Networks.BaseMainnet,
-];
-
 export default class NetworkSettings {
     ChainId?: number | string;
     DefaultPriorityFee?: number;
     BaseFeeMultiplier?: number;
-    AddressPlaceholder?: string;
-    OrderInDestination?: number;
-    OrderInSource?: number;
     AccountExplorerTemplate?: string;
     TransactionExplorerTemplate?: string;
     GasCalculationType?: GasCalculation
     isFeatured?: boolean
-    ChainOrder?: number
 
     public static KnownSettings: { [network: string]: NetworkSettings } = {};
 
@@ -83,7 +33,6 @@ export default class NetworkSettings {
             BaseFeeMultiplier: 1.7
         };
         NetworkSettings.KnownSettings[KnownInternalNames.Networks.EthereumMainnet] = {
-            ChainId: 1,
             isFeatured: true,
             AccountExplorerTemplate: 'https://etherscan.io/address/{0}',
             TransactionExplorerTemplate: 'https://etherscan.io/tx/{0}"',
@@ -91,7 +40,6 @@ export default class NetworkSettings {
             BaseFeeMultiplier: 1.7
         };
         NetworkSettings.KnownSettings[KnownInternalNames.Networks.EthereumSepolia] = {
-            ChainOrder: 1,
             TransactionExplorerTemplate: 'https://sepolia.etherscan.io/tx/{0}',
             AccountExplorerTemplate: 'https://sepolia.etherscan.io/address/{0}'
         };
@@ -128,17 +76,6 @@ export default class NetworkSettings {
             AccountExplorerTemplate: 'https://sepolia.voyager.online/contract/{0}',
         };
 
-
-        for (var k in NetworkSettings.KnownSettings) {
-            let networkSetting = NetworkSettings.KnownSettings[k];
-            if (networkSetting) {
-                let destOrder = destinationOrder.indexOf(k);
-                let srcOrder = sourceOrder.indexOf(k);
-
-                networkSetting.OrderInDestination = destOrder < 0 ? destinationOrder.length : destOrder;
-                networkSetting.OrderInSource = srcOrder < 0 ? destinationOrder.length : srcOrder;
-            }
-        }
     }
 }
 
