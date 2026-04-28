419b3cb39c42e2c396ff8fdda873f30837a05354
yasha-meursault <y.hayrumyan.y@gmail.com>
2026-04-28 21:44:28 +0400

    unify buttons styles and fix wallets button on mobiles

--- apps/app/components/Sidebar/WalletsDialog.tsx
+++ apps/app/components/Sidebar/WalletsDialog.tsx
@@ -2,8 +2,10 @@
 
 import { FC } from "react"
 import AppShellDialog from "@/components/shared/AppShellDialog"
+import VaulDrawer from "@/components/Modal/vaulModal"
 import WalletsList from "@/components/Wallet/WalletsList"
 import useWallet from "@/hooks/useWallet"
+import useWindowDimensions from "@/hooks/useWindowDimensions"
 
 interface WalletsDialogProps {
     open: boolean
@@ -12,6 +14,24 @@ interface WalletsDialogProps {
 
 const WalletsDialog: FC<WalletsDialogProps> = ({ open, onOpenChange }) => {
     const { wallets } = useWallet()
+    const { isMobile } = useWindowDimensions()
+
+    if (isMobile) {
+        return (
+            <VaulDrawer
+                show={open}
+                setShow={(show) => { if (!show) onOpenChange(false) }}
+                header="Connected wallets"
+                modalId="connected-wallets"
+                mode="fitHeight"
+            >
+                <VaulDrawer.Snap id="item-1">
+                    <WalletsList wallets={wallets} layout="overlay" />
+                </VaulDrawer.Snap>
+            </VaulDrawer>
+        )
+    }
+
     return (
         <AppShellDialog open={open} onOpenChange={onOpenChange} title="Connected wallets">
             <WalletsList wallets={wallets} layout="overlay" />
