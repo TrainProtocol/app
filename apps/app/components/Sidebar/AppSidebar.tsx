cb5a21094bb71d3a729fe228deefefa65d5df7e3
Aren <aren@bransfer.io>
2026-05-05 20:45:28 +0400

    Update padding in AppSidebar footer for improved layout

--- apps/app/components/Sidebar/AppSidebar.tsx
+++ apps/app/components/Sidebar/AppSidebar.tsx
@@ -85,7 +85,7 @@ const AppSidebar: FC = () => {
 
             <SidebarFooter className="gap-0 p-0">
                 <div className="border-t border-sidebar-border" />
-                <div className="px-2 pt-2">
+                <div className="px-2 py-2">
                     <SidebarLoginStatus />
                 </div>
                 {/* <div className="px-3 pt-1 pb-3">
